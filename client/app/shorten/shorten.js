cfb2311e954abe039d285d9898e19e4b1cc18bb8
jwdomingo <johnwill.domingo@gmail.com>
2016-02-12 16:38:44 -0800

    Toggle spinner on link fetch

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,10 +2,14 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
 	$scope.link = {};
-  $scope.addLink = function(url) {
+  $scope.spinner = false;
+  $scope.addLink = function (url) {
+    $scope.link = {};
+    $scope.spinner = true;
     Links.addOne(url)
-    .then(function(link) {
+    .then(function (link) {
       $scope.link = link.data;
+      $scope.spinner = false;
     });
   };
 });
