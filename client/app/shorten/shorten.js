0f80c585d94f414c66481ea96190cadc42a4d477
jwdomingo <johnwill.domingo@gmail.com>
2016-02-12 19:31:19 -0800

    Add basic styles and validate URL on client side

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,15 +1,29 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-	$scope.link = {};
+	$scope.serverMessage = "";
+  $scope.link = {};
   $scope.spinner = false;
-  $scope.addLink = function (url) {
+  $scope.addLink = function (url) {  
+    $scope.serverMessage = "";
     $scope.link = {};
     $scope.spinner = true;
     Links.addOne(url)
     .then(function (link) {
       $scope.link = link.data;
       $scope.spinner = false;
+    })
+    .catch(function (err) {
+      $scope.spinner = false;
+      $scope.serverMessage = err.data.error;
     });
   };
+
+  $scope.message = function() {
+    if ($scope.serverMessage) {
+      return $scope.serverMessage;
+    } else if ($scope.shortenForm.url.$invalid) {
+      return "Please enter a valid URL";
+    }
+  }
 });
