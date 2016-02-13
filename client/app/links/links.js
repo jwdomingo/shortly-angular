c6b70f4e5137969d413be7923b9419671eabfa25
John Domingo <johnwill.domingo@gmail.com>
2016-02-13 11:39:57 -0800

    Add live search that filters link list based on parent controller's search query

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,8 +2,12 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
+  $scope.searchString = $scope.$parent.serachString;
   Links.getAll()
   .then(function (links) {
     $scope.data.links = links;
   });
+})
+.controller('SearchController', function ($scope) {
+  $scope.search = {};
 });
